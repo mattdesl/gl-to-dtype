65d698c69f8f8fdda4250545947d9a9389f99ba6
mattdesl <dave.des@gmail.com>
2015-05-25 00:23:26 -0400

    bam!

---
+++ test.js
@@ -0,0 +1,15 @@
+var glToDtype = require('./')
+var test = require('tape')
+var gl = require('gl-constants')
+
+test('turns a gl type into a dtype string', function (t) {
+  t.equal(glToDtype(gl.FLOAT), 'float32')
+  t.equal(glToDtype(gl.UNSIGNED_BYTE), 'uint8')
+  t.equal(glToDtype(gl.UNSIGNED_SHORT), 'uint16')
+  t.equal(glToDtype(gl.UNSIGNED_INT), 'uint32')
+  t.equal(glToDtype(gl.BYTE), 'int8')
+  t.equal(glToDtype(gl.SHORT), 'int16')
+  t.equal(glToDtype(gl.INT), 'int32')
+  t.equal(glToDtype(gl.TEXTURE_2D), null, 'invalid types')
+  t.end()
+})
